e805d54232908f964a5d78e722ed7a7c286e39e7
LuisSubiabre <luis.subiabre@gmail.com>
2025-05-09 09:33:45 -0300

    feat: agregar imagen 404 y mejorar el componente NotFound con un diseño más claro

--- src/components/ComunicadosComponent.jsx
+++ src/components/ComunicadosComponent.jsx
@@ -10,7 +10,7 @@ export const ComunicadosComponent = () => {
     <div className="container">
       <div className="row">
         <div className="col-12">
-          <h2 className="mb-4">Últimos comunicados</h2>
+          <h2 className="mb-4">Últimos comunicados:</h2>
         </div>
       </div>
 
